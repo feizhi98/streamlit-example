973daae95ea877b638a41df5351352464730a4c9
feizhi98 <80804242+feizhi98@users.noreply.github.com>
2022-08-24 10:08:41 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -4,6 +4,7 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
+# Tan Fei Zhi
 # Simple Iris Flower Prediction App
 
 This app predicts the **Iris flower** type!
